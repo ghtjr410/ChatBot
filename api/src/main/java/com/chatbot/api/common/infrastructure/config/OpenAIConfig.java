69e75249880d87105f160b739c2587dd17f048f9
ghtjr410 <ghtjr410@gmail.com>
2025-03-14 18:56:01 +0900

    url 불러오는방식 수정

--- api/src/main/java/com/chatbot/api/common/infrastructure/config/OpenAIConfig.java
+++ api/src/main/java/com/chatbot/api/common/infrastructure/config/OpenAIConfig.java
@@ -1,19 +1,19 @@
 package com.chatbot.api.common.infrastructure.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
-@ConfigurationProperties(prefix = "openai.api")
 public class OpenAIConfig {
-    private String url;
+    @Value("${openai.api.url}")
+    private String apiUrl;
 
     @Bean
     public RestClient restClient() {
         return RestClient.builder()
-                .baseUrl(url)
+                .baseUrl(apiUrl)
                 .build();
     }
 }
